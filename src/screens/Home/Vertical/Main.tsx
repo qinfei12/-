2f4631403ce0328d72562a7fca48d1b5d1f77d06
qinfei12 <166202156+qinfei12@users.noreply.github.com>
2026-06-15 01:32:34 +0800

    Update Main.tsx

--- src/screens/Home/Vertical/Main.tsx
+++ src/screens/Home/Vertical/Main.tsx
@@ -375,6 +375,42 @@ const OneDrivePage = () => {
   return visible ? component : null
 }
 
+const WebDAVPage = () => {
+  const [visible, setVisible] = useState(commonState.navActiveId == 'nav_webdav')
+  const component = useMemo(() => <WebDAV />, [])
+  useEffect(() => {
+    let currentId: CommonState['navActiveId'] = commonState.navActiveId
+    const handleNavIdUpdate = (id: CommonState['navActiveId']) => {
+      currentId = id
+      if (id == 'nav_webdav') {
+        requestAnimationFrame(() => {
+          setVisible(true)
+        })
+      }
+    }
+    const handleHide = () => {
+      if (currentId != 'nav_setting') return
+      setVisible(false)
+    }
+    const handleConfigUpdated = (keys: Array<keyof LX.AppSetting>) => {
+      if (keys.some((k) => hideKeys.includes(k))) handleHide()
+    }
+    global.state_event.on('navActiveIdUpdated', handleNavIdUpdate)
+    global.state_event.on('themeUpdated', handleHide)
+    global.state_event.on('languageChanged', handleHide)
+    global.state_event.on('configUpdated', handleConfigUpdated)
+
+    return () => {
+      global.state_event.off('navActiveIdUpdated', handleNavIdUpdate)
+      global.state_event.off('themeUpdated', handleHide)
+      global.state_event.off('languageChanged', handleHide)
+      global.state_event.off('configUpdated', handleConfigUpdated)
+    }
+  }, [])
+
+  return visible ? component : null
+}
+
 const SettingPage = () => {
   const [visible, setVisible] = useState(commonState.navActiveId == 'nav_setting')
   const component = useMemo(() => <Setting />, [])
